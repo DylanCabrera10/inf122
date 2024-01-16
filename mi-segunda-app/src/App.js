85f51bef08307c162be3f5c0b3312573b4d51029
DylanCabrera10 <155450686+DylanCabrera10@users.noreply.github.com>
2024-01-16 13:03:24 +0000

    boton ocultar

--- mi-segunda-app/src/App.js
+++ mi-segunda-app/src/App.js
@@ -5,42 +5,49 @@ import { useState } from 'react';
 
 function App() {
   const [nroClicks, setNumClicks] = useState(0);
+  const [show, setShow] = useState(true);
   const click = () => {
     setNumClicks(nroClicks + 1);
+    console.log("click");
+  }
+  const clickMas3 = () => {
+    setNumClicks(nroClicks + 3);
+
+  }
+  const clickMenos3 = () => {
+    setNumClicks(nroClicks - 3);
+
+  }
+  const clickMenos = () => {
+    setNumClicks(nroClicks - 1);
+
   }
   const reiniciar = () => {
     setNumClicks(0);
+    console.log('reiniciar');
   }
-  const clickmenos3 = () => {
-    setNumClicks(nroClicks -3);
+  const mostrar = () => {
+    setShow(!show);
   }
-  const clickmas3 = () => {
-    setNumClicks(nroClicks +3);
-  }
-  const clickmenos1 = () => {
-    setNumClicks(nroClicks -1);
-  }
-  const clickmas1 = () => {
-    setNumClicks(nroClicks +1);
-  }
-  
   return (
     <div className="App">
       <div className="contenedor-principal">
-        <Contador nroClicks={nroClicks} />
+        <Contador nroClicks={nroClicks} mostrar={show} />
         <div className='contendedor-botones'>
-          <Boton texto="-3" esBotonClick={true}
-            funcionClick={clickmenos3} />
-          <Boton texto="+3" esBotonClick={true}
-            funcionClick={clickmas3} />
-          <Boton texto="-1" esBotonClick={true}
-            funcionClick={clickmenos1} />
-          <Boton texto="+1" esBotonClick={true}
-            funcionClick={clickmas1} />
-        </div>
-        <div className='contendedor-reiniciar'>
+          <div className='contendedor-botones-numeros'>
+            <Boton texto="-3" esBotonClick={true}
+              funcionClick={clickMenos3} />
+            <Boton texto="+3" esBotonClick={true}
+              funcionClick={clickMas3} />
+            <Boton texto="-1" esBotonClick={true}
+              funcionClick={clickMenos} />
+            <Boton texto="+1" esBotonClick={true}
+              funcionClick={click} />
+          </div>
+
           <Boton texto="Reiniciar" esBotonClick={false}
             funcionClick={reiniciar} />
+          <Boton texto="Mostrar/Ocultar" esBotonClick={false} funcionClick={mostrar} />
         </div>
       </div>
     </div>
